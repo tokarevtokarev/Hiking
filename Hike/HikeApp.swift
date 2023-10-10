65dd0fe48928f2afc591aa8b160098473f94688c
Bryan Lottes <bryan.lottes@epg.com>
2023-10-10 20:26:05 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Lottes, Bryan on 10.10.23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
